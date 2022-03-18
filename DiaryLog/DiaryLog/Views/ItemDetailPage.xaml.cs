f5de23f887b82235de897469f0fc008f89d84bab
Alex <Alex@DESKTOP-KPSSNL3>
2022-03-18 18:47:14 +0500

    Добавьте файлы проекта.

---
+++ DiaryLog/DiaryLog/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DiaryLog.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DiaryLog.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ DiaryLog/DiaryLog/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using DiaryLog.Models;
+using DiaryLog.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DiaryLog.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
